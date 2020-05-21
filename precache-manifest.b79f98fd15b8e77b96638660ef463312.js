b72149bb163381d0d6e90ce8951d4234012c5734
unknown <rjtsharma5452@gmail.com>
2020-05-21 11:17:24 +0530

    Updates

---
+++ precache-manifest.b79f98fd15b8e77b96638660ef463312.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95eef17dd469473cfdb89868e6a09bf5",
+    "url": "/Wemo/index.html"
+  },
+  {
+    "revision": "b366ef4d4523b8168205",
+    "url": "/Wemo/static/css/main.5e32d3d1.chunk.css"
+  },
+  {
+    "revision": "22b56cfc382ebe337262",
+    "url": "/Wemo/static/js/2.bc5102cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wemo/static/js/2.bc5102cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b366ef4d4523b8168205",
+    "url": "/Wemo/static/js/main.84387a0d.chunk.js"
+  },
+  {
+    "revision": "a062d4ab67f7bc40bf9d",
+    "url": "/Wemo/static/js/runtime-main.63e8a5bb.js"
+  },
+  {
+    "revision": "93ce1e761e7c9a65e71395a43f6f3cee",
+    "url": "/Wemo/static/media/cloud.93ce1e76.jpg"
+  },
+  {
+    "revision": "c7254e16e33968e6e5e9dd50a7908f9e",
+    "url": "/Wemo/static/media/sun.c7254e16.jpeg"
+  }
+]);
